1fa9cdbb9356516a6395b6bed7abc9f14cded65f
Marciel123 <116393215+Marciel123@users.noreply.github.com>
2022-11-02 22:15:04 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,34 +1,33 @@
-from sklearn.ensemble import RandomForestClassifier
+import streamlit as st
+
+from sklearn.ensemble import RandomForestClassifier 
 import pandas as pd
+dados = pd.read_csv('Iris.csv')
 
-
-dados=pd.read_csv Iris
-
-dados.head()
 classes = dados['Species']
 nomesColunas = dados.columns.to_list()
-tamanho = len(nomesColunas)#quantos nomes tem
-nomesColunas = nomesColunas[1:tamanho-1] #retira o ultimo
-features = dados[nomesColunas] #monta o features
-
-
+tamanho = len(nomesColunas)
+nomesColunas = nomesColunas[1:tamanho-1]
+features = dados[nomesColunas]
 
 from sklearn.model_selection import train_test_split
 
 features_treino,features_teste,classes_treino,classes_teste = train_test_split(features,
                                                                                classes,
-                                                                               test_size=0.6,
-                                                                               random_state=2)
-#criando a floresta
-floresta = RandomForestClassifier(n_estimators=1000) #constroi a floresta
+                                                                               test_size=0.26,
+                                                                               random_state=3)
 
-#treinar a floresta
+floresta = RandomForestClassifier(n_estimators=90) 
 
 floresta.fit(features_treino,classes_treino)
-
-#testar quanto a floresta acerta
 predicoes = floresta.predict(features_teste)
-#verificar quanto das predicoes foram acertos
-from sklearn import metrics
 
-print(metrics.classification_report(classes_teste,predicoes))
+
+st.title('Aplicativo de IA')
+SepalLengthCm = st.number_input('Digite o comprimento do caule')
+SepalWidthCm = st.number_input('Digite a largura do caule')
+PetalLengthCm = st.number_input('Digite o comprimento da petala')
+PetalWidthCm = st.number_input('Digite a largura da petala')
+if st.button('Clique aqui'):
+  resultado = floresta.predict([[SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm]])
+  st.write('Resultado:',resultado)
